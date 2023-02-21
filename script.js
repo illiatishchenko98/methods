b405489e966c1492221e0d3943220c5f8fb1fe28
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-02-20 21:40:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//str
+
+let fruit = 'qaswe jaaa asldsef';
+console.log(fruit.indexOf('j'));
+
+let text = 'lorem';
+console.log(text.toUpperCase(text));
+
+let test1 = 'LOREM';
+console.log(text.toLowerCase(text));
+
+
+let slice = 'Hello guys, I`m learning JS';
+console.log(slice.slice(25, 27));
+
+//num
+
+let num = 12.5;
+console.log(Math.round(num));
+
+const test = '12.5px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
